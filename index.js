1cd4926f4b8eaf482b9867a7674719739375afe3
N3v1 <n3v1010@gmail.com>
2023-07-30 11:32:04 +0200

    Calculate func

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function appendOperation(operation) {
     document.getElementById('resultArea').innerHTML +=operation;
 }
+
+function calcResult() {
+    let container = document.getElementById('resultArea');
+    let result = eval(container.innerHTML);
+
+    container.innerHTML = 'Hello, World';
+}
