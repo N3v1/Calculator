eaad074030434df49dce773d8530148ca23f4762
jiri132 <76393975+jiri132@users.noreply.github.com>
2023-08-05 14:08:38 +0200

    A small update on expacted % behavior

--- index.js
+++ index.js
@@ -9,10 +9,37 @@ function appendOperation(operation) {
 }
 
 function calculateResult() {
-    let container = document.getElementById('resultArea');
-    let result = eval(container.innerHTML);
+    // Changed it to a constant because you shouldn't be able to set this variable during running of the result
+    //  With a constant you can only set the object once so you can still mutate the object @jiri132
+    const container = document.getElementById('resultArea');
 
+    // Get the full calculation
+    const calculation = container.innerHTML;
+    const splitUpCalculation = calculation.split(" ");
+
+    if (splitUpCalculation.includes("%")) {
+        // Get the index and the number that is tied to the % value
+        const index = splitUpCalculation.indexOf("%");
+        const number = splitUpCalculation[index - 1];
+
+        // Remove the % index
+        splitUpCalculation.splice(index, 1);
+
+        // The past number of example `7% -> 7 -> 0,07`
+        splitUpCalculation[index - 1] = percentageToDecimal(number)
+    }
+
+    // Find the result using the new calculation method
+    let result = eval(splitUpCalculation.join(" "));
+
+    // set the calculation result
     container.innerHTML = result;
+}
+
+
+/// This gets the number input and makes it to decimal using the function `%value / 100 = decimal value` 
+function percentageToDecimal(percentage) {
+    return percentage / 100;
 }
 
 function deleteLast() {
