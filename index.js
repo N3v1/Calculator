438a27cfc1087be0554ce637403089455671f691
Ifeanyi Ofikulu <ifeanyiofi@gmail.com>
2023-08-13 12:14:55 +0100

    fixed the mobile responsive and percentage

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function appendOperation(operation) {
     operation === " + " ||
     operation === " - " ||
     operation === " * " ||
-    operation === " / "
+    operation === " / " ||
+    operation === "%"
   ) {
     if (lastInputIsOperator) {
       // Replace the last operator with the new one
@@ -39,48 +40,53 @@ function appendDecimal(decimal) {
   }
 }
 
+// Assuming you've included the decimal.js library in your HTML
+
+// Assuming you've included the decimal.js library in your HTML
+
+// Convert a percentage value to a decimal
+function percentageToDecimal(percentage) {
+  return new Decimal(percentage) / 100; // Divide the percentage value by 100 to get the decimal value
+}
+
+// Calculate and display the result of the expression
 function calculateResult() {
-  // Changed it to a constant because you shouldn't be able to set this variable during running of the result
-  // With a constant you can only set the object once so you can still mutate the object @jiri132
+  // Get containers for previous expression and result display
   const previousExpressionContainer =
     document.getElementById("previousExpression");
   const resultContainer = document.getElementById("resultArea");
 
+  // Get the expression from the result display
   const calculation = resultContainer.innerHTML;
-  const splitUpCalculation = calculation.split(" ");
+  const splitUpCalculation = calculation.split(" "); // Split the expression into parts
+
   let newCalculation = [];
 
-  // `part` represents the part of the calculation
-  // example: `[100 * 55% + 9 * 10%] => p1 = 100, p2 = *, p3 = 55, p4 = %, p[n]......`
-  // This function can also be used to find `sin(,cos(,tan(,root, exp, pi, ......`
+  // Iterate through the expression parts
   splitUpCalculation.forEach((part, index) => {
-    // Is the part a %?
     if (part === "%") {
-      // Get the number associated with %
-      const number = splitUpCalculation[index - 1];
+      // If the part is a percentage symbol
+      const number = splitUpCalculation[index - 1]; // Get the number before the percentage symbol
       const nC_index = newCalculation.length - 1;
 
-      // instead of the normal number place the decimal value there
-      newCalculation[nC_index] = percentageToDecimal(number);
-
-      // Return it so that it doesn't add the % back into it
+      // Check if the number is already a decimal or needs conversion
+      if (typeof number === "number") {
+        newCalculation[nC_index] = number / 100; // Convert the number to a decimal
+      } else {
+        newCalculation[nC_index] = percentageToDecimal(number); // Convert the percentage value to a decimal
+      }
       return;
     }
 
-    // If it is not a % just push the part that is in it
-    newCalculation.push(part);
+    newCalculation.push(part); // Push non-percentage parts to the newCalculation array
   });
 
-  const previousExpression = splitUpCalculation.join(" ");
+  const previousExpression = splitUpCalculation.join(" "); // Join the parts back to the expression
   previousExpressionContainer.innerHTML = previousExpression;
 
-  let result = eval(newCalculation.join(" "));
-  resultContainer.innerHTML = result;
-}
-
-// This gets the number input and makes it a decimal using the function `%value / 100 = decimal value` @jiri132
-function percentageToDecimal(percentage) {
-  return percentage / 100;
+  // Use Decimal type for calculations
+  let result = new Decimal(eval(newCalculation.join(" "))); // Evaluate the new expression
+  resultContainer.innerHTML = result.toString(); // Convert result to string for display
 }
 
 function deleteLast() {
